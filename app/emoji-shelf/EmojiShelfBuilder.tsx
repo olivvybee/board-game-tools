bf57a7d05b1b8548775ce68981726b4930ae8d81
Liv Flowers <oliviaflowers93@gmail.com>
2024-11-02 00:50:44 +0000

    Add note about entering emojis

--- app/emoji-shelf/EmojiShelfBuilder.tsx
+++ app/emoji-shelf/EmojiShelfBuilder.tsx
@@ -68,26 +68,8 @@ export const EmojiShelfBuilder = () => {
     fontSize,
   });
 
-  console.log(shelf);
-
   return (
     <>
-      <h1>Emoji shelf builder</h1>
-
-      <p>
-        An easy way to generate a shelf for{' '}
-        <a href="https://boardgamegeek.com/geeklist/344710/solitaire-games-on-your-table-november-2024">
-          SGOYT November 2024
-        </a>
-        . Simply choose the size of your shelf, enter an emoji into each
-        section, and copy the BGG code generated below.
-      </p>
-
-      <p>
-        If you use the same device each time you want to update your shelf, the
-        previous layout of your shelf will be saved when you come back.
-      </p>
-
       <div className={styles.config}>
         <div className={styles.configItem}>
           Rows
--- app/emoji-shelf/page.tsx
+++ app/emoji-shelf/page.tsx
@@ -1,6 +1,33 @@
 import { EmojiShelfBuilder } from './EmojiShelfBuilder';
 
-const EmojiShelfPage = () => <EmojiShelfBuilder />;
+const EmojiShelfPage = () => (
+  <>
+    <h1>Emoji shelf builder</h1>
+
+    <p>
+      An easy way to generate a shelf for{' '}
+      <a href="https://boardgamegeek.com/geeklist/344710/solitaire-games-on-your-table-november-2024">
+        SGOYT November 2024
+      </a>
+      . Simply choose the size of your shelf, enter an emoji into each section,
+      and copy the BGG code generated below.
+    </p>
+
+    <p>
+      If you use the same device each time you want to update your shelf, the
+      previous layout of your shelf will be saved when you come back.
+    </p>
+
+    <p>
+      If you're unsure how to enter emojis, you could either copy and paste them
+      from a website like <a href="https://emojipedia.org">Emojipedia</a>, or
+      you could enter letters as placeholders and then replace them in the BGG
+      interface using the emoji picker there.
+    </p>
+
+    <EmojiShelfBuilder />
+  </>
+);
 
 export default EmojiShelfPage;
 
